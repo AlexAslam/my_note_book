82c3bfa6157d0f9d17445e16e531d90251873f02
Alex Aslam <alexaslam02@hotmail.com>
2018-01-27 09:06:03 +0500

    dpeloyment rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.2.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-
+set :whenever_command, [:bundle, :exec, :whenever]
 set :bundle_flags, '--deployment'
 set :bundle_binstubs, nil
 
