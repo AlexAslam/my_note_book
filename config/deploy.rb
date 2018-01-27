3605daa2c2fdb01e43fd6e5dd88ef6685f54133c
Alex Aslam <alexaslam02@hotmail.com>
2018-01-27 09:39:34 +0500

    dpeloyment rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,7 +87,8 @@ namespace :deploy do
   # remove the default nginx configuration as it will tend
   # to conflict with our configs.
   before 'deploy:setup_config', 'nginx:remove_default_vhost'
-
+  before 'deploy:setup_database_config', 'nginx:remove_default_vhost'
+  
   # reload nginx to it will pick up any modified vhosts from
   # setup_config
   after 'deploy:setup_config', 'nginx:restart'
